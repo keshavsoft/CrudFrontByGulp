bd32d11069b8d069fd74c5dc83edb214c5635afd
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-01-21 13:05:13 +0530

    DependantTables Js

--- src/Js/CommonJs/V1/ForDependantTablesClass/FetchAsGet/entryFile.js
+++ src/Js/CommonJs/V1/ForDependantTablesClass/FetchAsGet/entryFile.js
@@ -1,6 +1,12 @@
-const StartFunc = () => {
-    console.log("sssssssss");
+import { StartFunc as StartFuncFetchFuncs } from './fetchFuncs.js';
+import { StartFunc as StartFuncFromAfterFetch } from './AfterFetch/entryFile.js';
 
+const StartFunc = async (e) => {
+    let jVarLocalTableName = e.target.innerText;
+    
+    let localResponse = await StartFuncFetchFuncs({ inTableName: jVarLocalTableName });
+
+    StartFuncFromAfterFetch({ inResponse: localResponse, inTableName: jVarLocalTableName });
 };
 
 export { StartFunc };
