5976e93ee4d8afdbd0f28d7f47413dbcd6aaad32
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-02-06 21:37:59 +0530

    Unwanted code removed , Added after fetch and Added new vertical dl

--- src/Js/Import/FromExcel/FormLoad/DOMContentLoaded/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/Js/Import/FromExcel/FormLoad/DOMContentLoaded/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,19 +1,29 @@
-import { StartFunc as Status200 } from "./Status200.js";
-import { StartFunc as Status500 } from "./Status500.js";
+import { StartFunc as Status200 } from "./status200.js";
+import { StartFunc as Status409 } from "./status409.js";
+import { StartFunc as Status401 } from "./status401.js";
+import { StartFunc as Status404 } from "./status404.js";
 
 let StartFunc = async ({ inResponse }) => {
-    let jVarLocalResponse = inResponse;
+    let jVarLocalResponse = await inResponse;
 
     if (jVarLocalResponse.status === 200) {
         let jVarLocalDataAsJson = await jVarLocalResponse.json();
-
         Status200({ inResponseAsJson: jVarLocalDataAsJson });
     };
 
-    if (jVarLocalResponse.status === 500) {
-        let jVarLocalFromResponse = await jVarLocalResponse.text();
+    if (jVarLocalResponse.status === 409) {
+        let jVarLocalSavedPk = await jVarLocalResponse.text();
+        Status409({ inResponse: jVarLocalSavedPk });
+    };
 
-        Status500({ inResponseAsText: jVarLocalFromResponse });
+    if (jVarLocalResponse.status === 401) {
+        let jVarLocalSavedPk = await jVarLocalResponse.text();
+        Status401({ inResponse: jVarLocalSavedPk });
+    };
+
+    if (jVarLocalResponse.status === 404) {
+        let jVarLocalSavedPk = await jVarLocalResponse.text();
+        Status404({ inResponse: jVarLocalSavedPk });
     };
 };
 
