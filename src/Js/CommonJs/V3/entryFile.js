286c1a355d89cf3139f7d83efd56c4ada5c3df5b
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-02-18 13:03:30 +0530

    Enter calculation

--- src/Js/CommonJs/V3/entryFile.js
+++ src/Js/CommonJs/V3/entryFile.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncForDependantTablesClass } from "./ForDependantTablesClass/entryFile.js";
+import { StartFunc as StartFuncForEnterKeyCalculation } from "./ForEnterKeyCalculation/entryFie.js";
 
 const StartFunc = () => {
     StartFuncForDependantTablesClass();
+    StartFuncForEnterKeyCalculation();
 };
 
 StartFunc();
